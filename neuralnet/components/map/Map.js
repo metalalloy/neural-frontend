e9c01b1e60228be73403408cadd5150bea49f524
Siah Wee Hung <siah_weehung@yahoo.com>
2024-04-22 02:40:33 -0400

    map component

---
+++ neuralnet/components/map/Map.js
@@ -0,0 +1,46 @@
+import { View } from "react-native"; 
+import { useEffect, useState } from "react";
+
+import MapView, { Polyline, Marker, PROVIDER_GOOGLE } from 'react-native-maps';
+
+import { fetch_positions } from "../../scripts/api";
+import styles from './style'
+
+const bus_icon = require("../../assets/bus-icon.png");
+
+export default function Map({lon, lat, path, route, dir, vars}) {
+    const [locs, setLocs] = useState();
+
+    useEffect(() => {
+        const refresh = setInterval(() => {
+            const fetch_locs = async () => {
+                const _locs = (await fetch_positions(vars, Array(vars.length).fill(dir), lon, lat))[route] ?? [];
+                setLocs(_locs);
+            }
+            fetch_locs();
+        }, 3000);
+
+        return () => {
+            clearInterval(refresh);
+        }
+    }, [])
+    return (
+        <View style={styles.mapContainer}> 
+            <MapView 
+                style={styles.map} 
+                provider={PROVIDER_GOOGLE}
+                initialRegion={{longitude: lon, latitude: lat, latitudeDelta: 0.02, longitudeDelta: 0.02}}
+            >
+                <Marker 
+                    coordinate={{longitude: lon, latitude: lat, latitudeDelta: 0.01, longitudeDelta: 0.01}}   
+                />
+                <Polyline
+                    coordinates={path}
+                    strokeColor="#e66"
+                    strokeWidth={6}
+                />
+                {locs && locs.map((loc, i) => <Marker key={i} coordinate={loc} image={bus_icon} />)}
+            </MapView>
+        </View>
+    )
+}
---
+++ neuralnet/components/map/style.js
@@ -0,0 +1,42 @@
+import { StyleSheet } from "react-native"; 
+
+export default styles = StyleSheet.create({ 
+	container: { 
+		display: "flex", 
+		alignContent: "center", 
+		alignItems: "center", 
+		justifyContent: "space-evenly", 
+		backgroundColor: "#ff0", 
+		height: "100%", 
+	}, 
+	heading: { 
+		fontSize: 28, 
+		fontWeight: "bold", 
+		marginBottom: 10, 
+		color: "red", 
+		textAlign: "center", 
+	}, 
+	heading2: { 
+		fontSize: 22, 
+		fontWeight: "bold", 
+		marginBottom: 10, 
+		color: "black", 
+		textAlign: "center", 
+	}, 
+	text1: { 
+		fontSize: 16, 
+		marginBottom: 10, 
+		color: "black", 
+		fontWeight: "bold", 
+	}, 
+	map: {
+		flex: 1,
+		borderWidth: 1,
+		width: "100%"
+	},
+	mapContainer: {
+		width: "100%",
+		height: 300,
+		borderWidth: 1,
+	}
+});

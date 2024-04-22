a064e4a2a6a41faf960990c14d63c2f6e099b378
Aloysius Ng <aloysius.ng2000@hotmail.com>
2024-04-21 21:52:00 -0500

    Modified API function call, response handling and styling for buttons, fonts

--- neuralnet/App.js
+++ neuralnet/App.js
@@ -7,10 +7,11 @@ import {
 	StyleSheet, 
 	Text, 
 	Image, 
-	SafeAreaView, 
+	SafeAreaView,
+	Pressable
 } from "react-native"; 
-import * as ImagePicker from "expo-image-picker"; 
-import * as Permissions from "expo-permissions";
+import * as ImagePicker from "expo-image-picker";
+import { fetch_stop, fetch_path, fetch_positions } from './scripts/api'
 
 export default function App() { 
 
@@ -19,10 +20,13 @@ export default function App() {
 	
 	// State to hold extracted text 
 	const [extractedText, setExtractedText] = 
-		useState(""); 
+		useState("");
 
-	// Function to pick an image from the 
-	// device's gallery 
+	// State while getting response from API
+	const [loading, setLoading] = useState(false);
+	const [response, setResponse] = useState(null);
+
+	// Function to pick an image from gallery 
 	const pickImageGallery = async () => { 
 		let result = 
 			await ImagePicker.launchImageLibraryAsync({ 
@@ -71,106 +75,116 @@ export default function App() {
 
 	// Function to perform OCR on an image 
 	// and extract text 
-	const performOCR = (file) => { 
-		let myHeaders = new Headers(); 
-		myHeaders.append( 
-			"apikey", 
-			
-			// ADDD YOUR API KEY HERE 
-			"FEmvQr5uj99ZUvk3essuYb6P5lLLBS20"
-		); 
-		myHeaders.append( 
-			"Content-Type", 
-			"multipart/form-data"
-		); 
-
-		let raw = file; 
-		let requestOptions = { 
-			method: "POST", 
-			redirect: "follow", 
-			headers: myHeaders, 
-			body: raw, 
-		}; 
-
-		// Send a POST request to the OCR API 
-		fetch( 
-			"https://api.apilayer.com/image_to_text/upload", 
-			requestOptions 
-		) 
-			.then((response) => response.json()) 
-			.then((result) => { 
-			
-				// Set the extracted text in state 
-				setExtractedText(result["all_text"]); 
-			}) 
-			.catch((error) => console.log("error", error)); 
+	const performOCR = async (file) => { 
+		try {
+			setLoading(true);
+			const response = await fetch_stop(1002864);
+			setResponse(response);
+			setExtractedText(response);
+		  } catch (error) {
+			console.error('Error fetching data:', error);
+		  } finally {
+			setLoading(false);
+		  }
 	}; 
+
+	const renderResponse = (obj, depth = 0) => {
+		return Object.keys(obj).sort().map((key, index) => {
+		  const value = obj[key];
+		  if (typeof value === 'object' && value !== null) {
+			return (
+			  <Text key={index} style={[styles.text, { marginLeft: depth * 10 }]}>
+				{key}:
+				{renderResponse(value, depth + 1)}
+			  </Text>
+			);
+		  } else {
+			return (
+			  <Text key={index} style={[styles.text, { marginLeft: depth * 10 }]}>
+				{`${key}: ${value}`}
+			  </Text>
+			);
+		  }
+		});
+	};
 
 	return ( 
 		<SafeAreaView style={styles.container}> 
 			<Text style={styles.heading}> 
 				WMATA Bus Timings Retriever 
 			</Text> 
-			<Text style={styles.heading2}> 
+			<Text style={styles.subheading}> 
 				Neural Networks & Deep Learning
 			</Text> 
-			<Button 
-				title="Choose from gallery"
-				onPress={pickImageGallery} 
-			/> 
-			<Button 
-				title="Use the Camera"
-				onPress={pickImageCamera} 
-			/> 
+			<Pressable style={styles.button} onPress={pickImageGallery}>
+        		<Text style={styles.buttonText}>Choose from gallery</Text>
+      		</Pressable>
+      		<Pressable style={styles.button} onPress={pickImageCamera}>
+        		<Text style={styles.buttonText}>Use the Camera</Text>
+      		</Pressable>
 			{image && ( 
 				<Image 
 					source={{ uri: image }} 
 					style={{ 
-						width: 400, 
-						height: 300, 
+						width: 200, 
+						height: 200, 
 						objectFit: "contain", 
 					}} 
 				/> 
-			)} 
-
-			<Text style={styles.text1}> 
-				Extracted text: 
-			</Text> 
-			<Text style={styles.text1}> 
-				{extractedText} 
-			</Text> 
+			)}
+			<Text style={styles.subheading2}>Bus Stop Information:</Text>
+      		{loading ? (
+        		<Text style={styles.text}>Loading...</Text>
+      		) : (
+        		response && renderResponse(response)
+      		)}
 			<StatusBar style="auto" /> 
 		</SafeAreaView> 
 	); 
-} 
+}
 
-const styles = StyleSheet.create({ 
-	container: { 
-		display: "flex", 
-		alignContent: "center", 
-		alignItems: "center", 
-		justifyContent: "space-evenly", 
-		backgroundColor: "#fff", 
-		height: "100%", 
-	}, 
-	heading: { 
-		fontSize: 28, 
-		fontWeight: "bold", 
-		marginBottom: 10, 
-		color: "red", 
-		textAlign: "center", 
-	}, 
-	heading2: { 
-		fontSize: 22, 
-		fontWeight: "bold", 
-		marginBottom: 10, 
-		color: "black", 
-		textAlign: "center", 
-	}, 
-	text1: { 
-		fontSize: 16, 
-		marginBottom: 10, 
-		color: "black", 
-		fontWeight: "bold", 
-	}, 
+const styles = StyleSheet.create({
+	container: {
+	  flex: 1,
+	  justifyContent: 'center',
+	  alignItems: 'center',
+	  backgroundColor: '#FFFFFF',
+	},
+	heading: {
+	  fontSize: 28,
+	  fontWeight: 'bold',
+	  marginBottom: 20,
+	  color: '#007AFF', 
+	},
+	subheading: {
+	  fontSize: 22,
+	  fontWeight: 'bold',
+	  marginBottom: 20,
+	  color: '#132676', // Black color for subheading
+	},
+	subheading2: {
+		fontSize: 16,
+		fontWeight: 'bold',
+		marginBottom: 20,
+		marginTop: 20,
+		color: '#132676', // Black color for subheading
+	  },
+	button: {
+		backgroundColor: '#000000',
+		marginVertical: 10,
+		marginBottom: 10,
+		paddingHorizontal: 20,
+		paddingVertical: 10,
+		borderRadius: 8,
+	  },
+	  buttonText: {
+		fontSize: 16,
+		fontWeight: 'bold',
+		color: '#FFFFFF', // White color for text
+	},
+	text: {
+	  fontSize: 14,
+	  marginBottom: 10,
+	  color: '#132676', // Black color for text
+	},
 });

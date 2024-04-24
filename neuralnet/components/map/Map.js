46409e3d7ad8e45102cb70373ab3086e3158d6ce
Siah Wee Hung <siah_weehung@yahoo.com>
2024-04-24 19:05:14 -0400

    loading spinner

--- neuralnet/components/map/Map.js
+++ neuralnet/components/map/Map.js
@@ -1,4 +1,4 @@
-import { View } from "react-native"; 
+import { View, ActivityIndicator } from "react-native"; 
 import { useEffect, useState } from "react";
 
 import MapView, { Polyline, Marker, PROVIDER_GOOGLE } from 'react-native-maps';
@@ -10,12 +10,15 @@ const bus_icon = require("../../assets/bus-icon.png");
 
 export default function Map({lon, lat, path, route, dir, vars}) {
     const [locs, setLocs] = useState();
-
+    const [loading, setLoading] = useState(true);
+    
     useEffect(() => {
+        setLoading(true);
         const refresh = setInterval(() => {
             const fetch_locs = async () => {
                 const _locs = (await fetch_positions(vars, Array(vars.length).fill(dir), lon, lat))[route] ?? [];
                 setLocs(_locs);
+                setLoading(false);
             }
             fetch_locs();
         }, 3000);
@@ -23,10 +26,11 @@ export default function Map({lon, lat, path, route, dir, vars}) {
         return () => {
             clearInterval(refresh);
         }
-    }, [])
+    }, [route, dir])
     return (
+        
         <View style={styles.mapContainer}> 
-            <MapView 
+            {!loading ? <MapView 
                 style={styles.map} 
                 provider={PROVIDER_GOOGLE}
                 initialRegion={{longitude: lon, latitude: lat, latitudeDelta: 0.02, longitudeDelta: 0.02}}
@@ -41,6 +45,9 @@ export default function Map({lon, lat, path, route, dir, vars}) {
                 />
                 {locs && locs.map((loc, i) => <Marker key={i} coordinate={loc} image={bus_icon} />)}
             </MapView>
+            :
+            <ActivityIndicator size="large" />
+            }
         </View>
     )
 }
